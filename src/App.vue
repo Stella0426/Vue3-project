d9bde8559d4dd3cc60a08b45a45e2f6e018b4a37
Byul <byul@Byului-MacBookPro.local>
2022-09-12 12:55:22 +0900

    About component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
+<!-- component : 하나의 부품을 의미함 따라서 여러곳에서 가져다 쓰일 수 있음 -->
+<!-- 뷰 컴포넌트 : 세개의 부분으로 나누어짐 
+  1. template(템플릿 안에는 html 코드가 들어감)
+  2. script(스크립트 안에는 js 로직이 들어감)
+  3. style(css 코드가 들어감)
+-->
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="name">{{ name }}</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  export default {
+    setup() {
+      const name = 'Kossie Coder1';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+      return{
+        name
+        // 변수를 리턴해주면 이 안의 변수들은 template안에서 접근이 가능해짐
+        // template 안에서 접근해주려면 {{데이터의 변수}} 콧수염문법 써주기
+      };
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .name{
+    color: red;
+  }
 </style>
